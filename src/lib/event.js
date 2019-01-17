8a937ba3c435033077d49e6aed3c99321d2df411
Jquiceno <loncuster@gmail.com>
2019-01-17 18:07:45 -0500

    Inicio de refator a Clase events

--- src/lib/event.js
+++ src/lib/event.js
@@ -1,31 +1,25 @@
+'use strict'
+
 import Db from './db'
 import Mailgun from './mailgun.js'
-import EventEmitter from 'events'
+import Boom from 'boom'
+import Message from '../message'
+import moment from 'moment'
 
-const emitter = new EventEmitter()
 const collection = 'events'
+const db = Db.init(collection)
 
 class Event {
   constructor (eventId) {
     this.id = eventId
-    this.db = Db.init(collection)
   }
 
-  static on (event, callback) {
-    return emitter.on(event, callback)
-  }
-
-  static emit (event, params) {
-    return emitter.emit(event, params)
-  }
-
-  static async getAllBy(key, value) {
+  static async getAllBy (key, value) {
     try {
-      const db = Db.init(collection)
       const ref = db
       let events = []
 
-      const query = await ref.where(key, '==', value).orderBy('_date.ut', 'desc').get()
+      const query = await ref.where(key, '==', value).orderBy('_created', 'desc').get()
 
       query.forEach(leadRef => {
         let eventData = leadRef.data()
@@ -35,42 +29,28 @@ class Event {
 
       return Promise.resolve(events)
     } catch (e) {
-      return Promise.reject({
-        error: {
-          message: e.details
-        },
-        status_code: 404,
-      })
+      return Promise.reject(new Boom(e))
     }
   }
 
-  async getData () {
+  async get () {
     let event
 
     try {
-      const eventRef = this.db.doc(this.id)
+      const eventRef = db.doc(this.id)
       event = await eventRef.get()
       event = event.data()
       event.id = eventRef.id
     } catch (e) {
-      return Promise.reject({
-        error: {
-          message: 'There was an error getting the required event',
-          provider: e.message
-        },
-        status_code: 500,
-      })
+      return Promise.reject(new Boom(e))
     }
 
     return Promise.resolve(event)
   }
 
   static async add (data) {
-    const Message = require('./message')
-    const db = Db.init(collection)
-    const timestamp = data.timestamp || Date.now() / 1000
-    const date = new Date(timestamp * 1000)
-    let ref = await db.doc()
+    const timestamp = moment().unix()
+    let ref = db.doc()
     let message = null
 
     try {
@@ -103,24 +83,15 @@ class Event {
           data['message-id'] = data['message-id'].substr(0, data['message-id'].length - 1).substr(1)
         }
 
-        message = await Message.getAllBy('service.id', data['message-id'])
+        message = await Message.getAllBy('provider.id', data['message-id'])
         if (message.length <= 0) {
           throw new Error('The event does not belong to a valid message')
         }
 
         data.message = message[0].ID
       }
-    } catch (e) {
-      return Promise.reject({
-        error: {
-          message: e.message,
-        },
-        status_code: 404
-      })
-    }
 
-    try {
-      const event = await ref.set({
+      await ref.set({
         message: data.message,
         event: data.event,
         city: data.city || null,
@@ -128,28 +99,22 @@ class Event {
         region: data.region || null,
         ip: data.ip || null,
         domain: data.domain || null,
-        'device-type': data['device-type'] || null,
+        device: {
+          type: data['device-type'] || null
+        },
         country: data.country || null,
-        'client-type': data['client-type'] || null,
-        'client-os': data['client-os'] || null,
-        'client-name': data['client-name'] || null,
+        client: {
+          type: data['client-type'] || null,
+          os: data['client-os'] || null,
+          name: data['client-name'] || null
+        },
         'user-agent': data['user-agent'] || data['User-Agent'] || null,
-        _date: {
-          ut: timestamp,
-          y: date.getFullYear(),
-          m: date.getMonth() + 1,
-          d: date.getDate()
-        }
+        _created: timestamp
       })
 
       return Promise.resolve(ref.id)
     } catch (e) {
-      return Promise.reject({
-        error: {
-          message: 'Error almacenando el evento enviado'
-        },
-        status_code: 500
-      })
+      return Promise.reject(new Boom(e))
     }
   }
 }
